bd564b750459316dc44fa49f2cc0134c1e7288cb
Soumye <singhalsoumye@gmail.com>
2019-12-04 01:29:17 -0500

    pp

--- packages/pure_pursuit/scripts/pure_pursuit_node.py
+++ packages/pure_pursuit/scripts/pure_pursuit_node.py
@@ -5,6 +5,67 @@ from duckietown_msgs.msg import SegmentList
 from duckietown_msgs.msg import Twist2DStamped 
 import numpy as np
 
+def callback_orig(data):
+    """
+    The algorithm to follow should be roughly the following:
+
+    Filter the line detections to find the white ones and yellow ones that are "close" to your lookahead distance L
+    Use these line detections to calculate an estimate of \alpha (I suggest to average between the white and yellow ones)
+    Encapsulate this in a node, wire things up, and try it on the robot.
+
+    """
+    # rospy.loginfo(rospy.get_caller_id() + 'I heard %s', data.data)
+    print('Call back Pure Pursuit')
+    # print('len', len(data.segments), data.segments[0].points)
+    n = len(data.segments)
+    white = []
+    yellow = []
+    for seg in data.segments:
+        if seg.color == 0:
+            white.append(seg.points)
+        elif seg.color == 1:
+            yellow.append(seg.points)
+        
+    if len(white) == 0 and len(yellow) == 0:
+        x = .1
+        y = 0
+    elif len(white)>=len(yellow):
+        x = 0
+        y = 0
+        for b in white:
+            x += (b[0].x + b[1].x)/2
+            y += (b[0].y + b[1].y)/2
+        x = x/len(white)
+        y = y/len(white)
+        y = y + .15
+    elif len(yellow) > len(white):
+        print("follow yellow")
+        x = 0
+        y = 0
+        for b in yellow:
+            x += (b[0].x + b[1].x)/2
+            y += (b[0].y + b[1].y)/2
+        x = x/len(yellow)
+        y = y/len(yellow)
+        y = y - .15
+    else:
+        x = .1
+        y = 0
+    print("x, y",x, y)
+    L = np.sqrt(x**2 + y**2)
+    alpha = np.arctan2(y,x)
+    v = 0.25
+    og = 2
+
+    omega = 2*v*(np.sin(alpha))/L
+    omega = omega*og
+
+    msg = Twist2DStamped()
+    msg.v = v
+    msg.omega = omega
+    print('sending: v, omega', v, omega)
+    pub.publish(msg)
+
 def callback(data):
     """
     The algorithm to follow should be roughly the following:
@@ -26,7 +87,10 @@ def callback(data):
         elif seg.color == 1:
             yellow.append(seg.points)
     
-    if len(white)>len(yellow):
+    if len(white) == 0 and len(yellow) == 0:
+        x = .1
+        y = 0
+    elif len(white)>=len(yellow):
         x = 0
         y = 0
         for b in white:
@@ -36,6 +100,7 @@ def callback(data):
         y = y/len(white)
         y = y + .15
     elif len(yellow) > len(white):
+        print("follow yellow")
         x = 0
         y = 0
         for b in yellow:
@@ -47,15 +112,29 @@ def callback(data):
     else:
         x = .1
         y = 0
+    print("x, y",x, y)
     L = np.sqrt(x**2 + y**2)
     alpha = np.arctan2(y,x)
     v = 0.2
+    og = 4
+    if np.abs(x) > 0.23:
+        print("gofast")
+        v = 0.8
+        og = 1.0
+
+    # v = 0.2
+    # og = 3.5  
+    # if np.abs(x) > 0.2:
+    #     v = 8.0
+    #     og = 1/2
     omega = 2*v*(np.sin(alpha))/L
+    omega = omega*og
+
 
     msg = Twist2DStamped()
-    print('sending: v, omega', v, omega)
     msg.v = v
-    msg.omega = omega*3.8
+    msg.omega = omega
+    print('sending: v, omega', v, omega)
     pub.publish(msg)
 
 
@@ -66,13 +145,18 @@ def listener():
     # anonymous=True flag means that rospy will choose a unique
     # name for our 'listener' node so that multiple listeners can
     # run simultaneously.
-    rospy.init_node('pure_pursuit_node', anonymous=True)
+    # rospy.init_node('pure_pursuit_node', anonymous=True)
 
-    rospy.Subscriber('/walle/lane_filter_node/seglist_filtered', SegmentList, callback)
+    # rospy.Subscriber('/default/lane_filter_node/seglist_filtered', SegmentList, callback)
+    rospy.Subscriber('~seglist_filtered', SegmentList, callback_orig)
+
 
     # spin() simply keeps python from exiting until this node is stopped
     rospy.spin()
 
 if __name__ == '__main__':
-    pub = rospy.Publisher('/walle/joy_mapper_node/car_cmd', Twist2DStamped, queue_size=10)
+    rospy.init_node('pure_pursuit_node', anonymous=True)
+    # pub = rospy.Publisher('/default/joy_mapper_node/car_cmd', Twist2DStamped, queue_size=10)
+    pub = rospy.Publisher('~car_cmd', Twist2DStamped, queue_size=10)
+
     listener()
